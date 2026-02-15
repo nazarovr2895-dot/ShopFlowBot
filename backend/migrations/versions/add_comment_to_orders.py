7beb817a199edc31049ea2835132476385f05513
nazarovr2895-dot <nazarovrustam20052gmail.com>
2026-02-15 23:28:40 +0300

    новый ui окна оформления заказа 5

---
+++ backend/migrations/versions/add_comment_to_orders.py
@@ -0,0 +1,28 @@
+"""add comment to orders
+
+Revision ID: add_comment_orders
+Revises: add_address_name
+Create Date: 2026-02-15 20:10:00.000000
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'add_comment_orders'
+down_revision: Union[str, None] = 'add_address_name'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    # Add comment column to orders table
+    op.add_column('orders', sa.Column('comment', sa.Text(), nullable=True))
+
+
+def downgrade() -> None:
+    # Remove comment column from orders table
+    op.drop_column('orders', 'comment')
